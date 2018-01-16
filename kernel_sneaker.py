4d1681dfd77fe5b35d5b0896ba5af985d43daf61
valexby <realnats@gmail.com>
2018-01-16 22:38:42 +0300

    Download any type of scripts

--- kernel_sneaker.py
+++ kernel_sneaker.py
@@ -3,21 +3,40 @@
 import requests
 import sys
 
-
-def get_python_id():
+def get_python_id(language, ace_language, notebook):
+    TOKEN_LEN = 1000
+    params = {
+    'sortBy': 'hotness',
+    'group': 'everyone',
+    'pageSize': TOKEN_LEN,
+    }
+    init = True
     init_url = 'https://www.kaggle.com/kernels/all/0'
     response = requests.get(init_url)
-    for kernel in response.json():
-        if kernel['languageName'] == 'Python':
-            return kernel['scriptVersionId']
+    while True:
+        for kernel in response.json():
+            if kernel['languageName'] == language \
+               and kernel['aceLanguageName'] == ace_language \
+               and kernel['isNotebook'] == notebook:
+                return kernel['scriptVersionId']
+        if init:
+            init = False
+        elif len(response.json()) < TOKEN_LEN:
+            return None
+        params['after'] = response.json()[-1]['id']
+        response = requests.get('https://www.kaggle.com/kernels.json', params=params)
 
 def main():
-    if len(sys.argv) < 2:
-            print("Put output filename in args please")
-            return 1
+    if len(sys.argv) < 5:
+        print("./sript.py <output_file> <language> <ace_language> <notebook>")
+        return 1
 
     out_filename = sys.argv[1]
-    download_url = 'https://www.kaggle.com/kernels/sourceurl/{}'.format(get_python_id())
+    language = sys.argv[2]
+    ace_language = sys.argv[3]
+    notebook = sys.argv[4] == 'true'
+
+    download_url = 'https://www.kaggle.com/kernels/sourceurl/{}'.format(get_python_id(language, ace_language, notebook))
     r = requests.get(download_url)
     r = requests.get(r.content)
     f = open(out_filename, 'wb')

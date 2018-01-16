69a1f13824f235fb1e7d7ae908e98803fd4b18f1
valexby <realnats@gmail.com>
2018-01-16 22:52:53 +0300

    Demo for python and python notebook kernels

--- demo.sh
+++ demo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-rm out/*
-./kernel_sneaker.py out/out.py
-pipreqs out/
-less out/requirements.txt
+rm py_out/*
+./kernel_sneaker.py py_out/out.py Python python false
+pipreqs py_out/
+
+rm py_notebook_out/*
+./kernel_sneaker.py py_notebook_out/out.ipynb Python python true
+jupyter nbconvert --to script py_notebook_out/out.ipynb
+pipreqs py_notebook_out/

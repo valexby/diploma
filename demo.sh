aa49e5d1abe60d0e6be2b3450e1c7e8326a61707
Aliaksandr Vasiuk <aliaksandr_vasiuk@epam.com>
2018-01-17 14:51:40 +0300

    R markdown and R notebook demos

--- demo.sh
+++ demo.sh
@@ -24,3 +24,23 @@ else
 fi
 ./kernel_sneaker.py r_out/out.R R r false
 grep -oP 'library\(\K[\w\s]+' r_out/out.R > r_out/requirements.txt
+
+
+if [ -d r_markdown_out ]; then
+    rm r_markdown_out/*
+else
+    mkdir r_markdown_out
+fi
+./kernel_sneaker.py r_markdown_out/out.Rmd R markdown false
+R -e "library(knitr);purl(\"r_markdown_out/out.Rmd\")"
+mv out.R r_markdown_out/out.R
+grep -oP 'library\(\K[\w\s]+' r_markdown_out/out.R > r_markdown_out/requirements.txt
+
+if [ -d r_notebook_out ]; then
+    rm r_notebook_out/*
+else
+    mkdir r_notebook_out
+fi
+./kernel_sneaker.py r_notebook_out/out.ipynb R r true
+jupyter nbconvert --to script r_notebook_out/out.ipynb
+grep -oP 'library\(\K[\w\s]+' r_notebook_out/out.r > r_notebook_out/requirements.txt
